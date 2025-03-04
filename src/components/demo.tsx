cb86b6b7de9c0781b7490f4b186b084675d193c3
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-05 00:59:50 +0800

    Refactor Demo component and update styling; remove unused imports and adjust layout in globals.css

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { Card, CardContent, CardHeader } from "./ui/card";
+// import { useState } from "react";
+import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { Download } from "lucide-react";
 import { Input } from "./ui/input";
 
 export default function Demo() {
-  const [inputValue, setInputValue] = useState("");
+  // const [inputValue, setInputValue] = useState("");
 
   return (
     <section id="demo" className="min-h-screen max-w-4xl mx-auto">
@@ -15,13 +15,14 @@ export default function Demo() {
       </h2>
       <div className="flex justify-center items-center mt-5">
         <Card className="w-full">
-          {/* <CardHeader className=""></CardHeader> */}
           <CardContent className="p-10">
-            <div className=" bg-gray-800 text-white text-sm p-2 rounded w-fit ">
-              <p>/Atomic Habits</p>
+            <div className="bg-gray-800 text-white text-sm p-2 rounded w-fit chat chat-start">
+              <div className="chat-bubble">
+                <p>/Atomic Habits</p>
+              </div>
             </div>
-            <div className="flex items-center border border-gray-300 rounded px-2 py-1 w-fit">
-              <span className="mr-2">AtomicHabits.pdf</span>
+            <div className="flex items-center border border-gray-300 rounded px-2 py-1 w-fit chat chat-end">
+              <span className="chat-bubble mr-2">AtomicHabits.pdf</span>
               <Download className="h-4 w-4 cursor-pointer" />
             </div>
 
@@ -29,8 +30,8 @@ export default function Demo() {
               <Input
                 type="text"
                 className="flex-grow rounded-r-none"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
+                // value={inputValue}
+                // onChange={(e) => setInputValue(e.target.value)}
               />
               <Button className="bg-gray-700 rounded-l-none">Send</Button>
             </div>
