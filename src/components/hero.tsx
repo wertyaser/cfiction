421d4031fd836722a26afb79ca5e926c9de82dfe
leonielNacman <nacmanleonielgarin@gmail.com>
2025-03-07 18:55:53 +0800

    Update Hero component text styling for improved responsiveness

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
       className="min-h-[50vh] flex flex-col items-center justify-center pt-20 pb-5 text-center"
     >
       <h1 className="text-8xl font-bold">Ctrl+F[iction]</h1>
-      <p className="mt-4 max-w-3xl text-xl font-light leading-relaxed">
+      <p className="mt-4 max-w-3xl text-xl font-light leading-relaxed md:text-md sm:text">
         A cutting-edge <b>AI chatbot</b> designed to effortlessly find and
         download <b>ebooks</b>,<b> publications</b>, <b>articles</b>, and
         <b>journals</b> with simple commands.
