935686e7e132565ec2799c76faec5ab49448e4bb
leonielNacman <nacmanleonielgarin@gmail.com>
2025-03-07 18:36:21 +0800

    Update ContactUs component layout and styling; enhance user input fields and improve Hero component text formatting

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -1,11 +1,36 @@
+import { Card, CardContent } from "./ui/card";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
 export default function ContactUs() {
   return (
     <div id="contact-us" className=" max-w-4xl mx-auto mt-20">
-      <h2 className="text-7xl font-bold text-center">
-        <span>C</span>
-        <span className="font-thin">ontact Us</span>
+      <h2 className="text-5xl font-bold text-center">
+        <span className="font-semibold">Contact Us</span>
       </h2>
-      <div className="flex justify-center items-center"></div>
+      <div className="flex justify-center items-center">
+        <span className="text-xl font-thin leading-relaxed text-center max-w-3xl">
+          Got a technical issue? Want to send feedback about a beta feature?
+          <br />
+          Need details about our Business plan? Let us know.
+        </span>
+      </div>
+      <Card className="max-w-4xl mx-auto mt-5 p-5">
+        <CardContent>
+          <div className="grid grid-cols-2 gap-5">
+            <div className="">
+              <Label>First Name</Label>
+              <Input type="text" placeholder="ex. John" />
+              <Label>First Name</Label>
+              <Input type="text" placeholder="ex. John" />
+            </div>
+            <div className="">
+              <Label>Last Name</Label>
+              <Input type="text" placeholder="" />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
       <h1 className="text-8xl font-bold">Ctrl+F[iction]</h1>
       <p className="mt-4 max-w-3xl text-xl font-light leading-relaxed">
         A cutting-edge <b>AI chatbot</b> designed to effortlessly find and
-        download <b>ebooks</b>,<b> publications</b>, <b>articles</b>, and{" "}
+        download <b>ebooks</b>,<b> publications</b>, <b>articles</b>, and
         <b>journals</b> with simple commands.
         <b> Why waste time browsing</b> when you can access instant results
         through a free ebooks API?
