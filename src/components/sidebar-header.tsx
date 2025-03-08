5cd95558c41dc538e616df561db6bbd1f9a4bc6e
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-09 03:34:01 +0800

    Refactor SideHeader component to improve layout and remove duplicate label

--- src/components/sidebar-header.tsx
+++ src/components/sidebar-header.tsx
@@ -5,12 +5,12 @@ import { Label } from "./ui/label";
 
 export default function SideHeader() {
   return (
-    <SidebarHeader className="">
+    <SidebarHeader className="flex items-center justify-between p-4">
+      <Label>Leoniel Nacman</Label>
       <Avatar className="">
         <AvatarImage src="https://github.com/shadcn.png" width={5} height={5} />
         <AvatarFallback>SC</AvatarFallback>
       </Avatar>
-      <Label>Leoniel Nacman</Label>
     </SidebarHeader>
   );
 }
