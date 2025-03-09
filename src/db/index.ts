80f2fe01f29324a3c0c943d57986094bbfe04f9b
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-10 01:12:01 +0800

    Implement authentication using better-auth; configure database connection with drizzle; add admin page for managing posts and users; define database schema for users and posts

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
+import { createClient } from "@libsql/client";
+
+config({ path: ".env" }); // or .env.local
+
+const client = createClient({
+  url: process.env.TURSO_CONNECTION_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+
+export const db = drizzle(client, { schema });
