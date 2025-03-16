9916e4c741de455b5a35297ebede168b741cd2ae
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-16 23:47:08 +0800

    Update EbookFinder component background color and import db in auth module

--- src/components/ebook-finder.tsx
+++ src/components/ebook-finder.tsx
@@ -4,7 +4,7 @@ import { Card } from "./ui/card";
 export default function EbookFinder() {
   return (
     <div className="flex justify-center items-center mt-5">
-      <Card className="h-[90vh] w-full max-w-5xl md:w-[1000px] sm:w-[900px] shadow-lg bg-primary">
+      <Card className="h-[90vh] w-full max-w-5xl md:w-[1000px] sm:w-[900px] shadow-lg bg-background">
         <h1>hello World</h1>
       </Card>
     </div>
