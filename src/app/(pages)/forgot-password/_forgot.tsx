0525da74d9a22caff6aa1e58a320f360c8c29921
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-24 21:33:23 +0800

    ADD forgotpassword component

---
+++ src/app/(pages)/forgot-password/_forgot.tsx
@@ -0,0 +1,89 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+import { useActionState, useEffect } from "react";
+import { forgotPassword, EmailState } from "@/actions/validation";
+import { useToast } from "@/hooks/use-toast";
+import { useFormStatus } from "react-dom";
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button disabled={pending} type="submit">
+      {pending ? "Sending..." : "Reset Password"}
+    </Button>
+  );
+}
+
+export default function ForgotPassword() {
+  const [state, action] = useActionState<EmailState | undefined, FormData>(
+    forgotPassword,
+    undefined
+  );
+  const { toast } = useToast();
+
+  // Show success toast when form submission is successful
+  useEffect(() => {
+    // if (state?.success) {
+    //   toast({
+    //     title: "Success",
+    //     description: "A password reset link has been sent to your email.",
+    //   });
+    // }
+
+    // Show error toast when there's an error
+
+    const errorMessage = state?.errors?.email?.[0] || state?.errors?.form?.[0];
+    if (errorMessage) {
+      toast({
+        variant: "destructive", // Make it red for errors
+        title: "Error",
+        description: errorMessage,
+      });
+    }
+  }, [state, toast]);
+
+  return (
+    <div>
+      <Button asChild variant="outline" className="ml-4 mt-4">
+        <Link href="/">
+          <ChevronLeft />
+          Home
+        </Link>
+      </Button>
+      <div className="flex flex-col items-center justify-center gap-16">
+        <Card className=" w-full max-w-3xl">
+          <CardHeader>
+            <h1 className="text-2xl text-center">Forgot Password</h1>
+            <CardDescription className="text-center">
+              Enter your email address below and we&apos;ll send you a link to
+              reset your password.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form action={action} className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
+                <Input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                  required
+                />
+              </div>
+              <SubmitButton />
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
