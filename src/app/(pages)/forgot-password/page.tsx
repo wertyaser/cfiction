b634f17a32ad7a0db0842f9ea7d94ceae932d034
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-25 16:19:41 +0800

    CHANGE layout of forgot password page for improved centering

--- src/app/(pages)/forgot-password/page.tsx
+++ src/app/(pages)/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import ForgotPassword from "./_forgot";
 
 export default function ForgotPasswordPage() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex items-center justify-center">
       <ForgotPassword />
     </div>
   );
