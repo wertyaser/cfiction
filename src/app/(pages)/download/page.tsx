36cf752abe4826746dbbf831ac617bb126309293
wertyaser <nacmanleonielgarin@gmail.com>
2025-03-26 02:29:26 +0800

    ADD new DownloadPage component for handling downloads

---
+++ src/app/(pages)/download/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+export default function DownloadPage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const title = searchParams.get("title");
+  const url = searchParams.get("url");
+
+  useEffect(() => {
+    if (title && url) {
+      console.log(`User clicked: ${title}`);
+      window.location.href = url;
+    }
+  }, [title, url]);
+
+  return (
+    <div className="text-center p-6">
+      <p>Redirecting to download...</p>
+    </div>
+  );
+}
