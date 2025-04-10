44286868534e78261387d3428ce5e46b961d8c10
wertyaser <nacmanleonielgarin@gmail.com>
2025-04-10 22:37:49 +0800

    UPDATE: Update Facebook link in social media section

--- src/components/home/contact-us.tsx
+++ src/components/home/contact-us.tsx
@@ -19,7 +19,7 @@ type SocialMedia = {
 const socialMedia: SocialMedia[] = [
   {
     name: "Facebook",
-    link: "https://www.facebook.com/",
+    link: "https://www.facebook.com/LeonielNacman0",
     icon: <Facebook />,
   },
   {
