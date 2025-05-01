b520c2925971d9a3e6feccb8c337d88f44b2638b
wertyaser <nacmanleonielgarin@gmail.com>
2025-05-01 16:35:19 +0800

    UPDATE: Refactor Hero component layout for improved structure and clarity

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,13 +1,20 @@
+import { X } from "lucide-react";
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <section
       id="home"
       className="min-h-[50vh] flex flex-col items-center justify-center pt-20 pb-5 text-center px-4 sm:px-6 lg:px-8">
-      <h1 className="text-4xl sm:text-6xl lg:text-8xl font-bold">Ctrl+F[iction]</h1>
+      <div className="flex gap-3">
+        <h1 className="text-4xl sm:text-6xl lg:text-8xl font-bold">Ctrl+F[iction]</h1>
+        <X size={100} />
+        <Image src={"/rtulogo.png"} width={100} height={100} alt="RTU logo" />
+      </div>
       <p className="mt-4 max-w-3xl text-base sm:text-lg lg:text-xl font-light leading-relaxed">
         Unleash a world of <b>ebooks</b> with lightning-fast searches across top libraries and chat
         with <b>Ctrl</b>, your <b>AI Librarian</b>, for instant answers. Discover, download, and
-        dive in—reading has never been this smart or simple!
+        dive in—searching has never been this smart or simple!
       </p>
     </section>
   );
