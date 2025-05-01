8ba8566db38ab9db511518e95a3e1a16b41c249f
wertyaser <nacmanleonielgarin@gmail.com>
2025-05-01 16:35:11 +0800

    UPDATE: Add university name to footer for branding consistency

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-4 text-center bg-background mt-10">
-      <p>© 2025 Ctrl + Fiction</p>
+      <p>© 2025 Ctrl + Fiction | Rizal Technological University</p>
     </footer>
   );
 }
